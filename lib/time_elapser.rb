3bc80a12dba789140a8fc976b1e51d0c65b21331
Edwin Cruz <edwin.cruz@crowdint.com>
2010-12-02 16:56:49 -0800

    Adding more test coverage and updating documentation

--- lib/time_elapser.rb
+++ lib/time_elapser.rb
@@ -1,4 +1,19 @@
+##
+ # TimeElapser
+ # Returns time in windows, making easier to query caching to save results
+ ##
 class TimeElapser
+
+  # Saninitize current time and returns an starting point of time every interval (in seconds)
+  #
+  # *Example*
+  #
+  # If you want to cache results for 5 seconds, do this:
+  # 
+  # @products = Product.find(:all, :conditions => ['available_date <= ?', TimeElapser.sanitize(5)])
+  #
+  # If RAILS_ROOT is defined, it will use Time.zone.now, otherwise it will use Time.now
+  #
   def self.sanitize interval = 1
     now = (defined?(RAILS_ROOT)) ? Time.zone.now : Time.now
     if interval > 1
--- spec/time_elapser_spec.rb
+++ spec/time_elapser_spec.rb
@@ -5,21 +5,50 @@ describe TimeElapser do
   subject { TimeElapser }
 
   context 'time elapser initialized without parameters' do
-    
     it 'should return the current date if no params are provided' do
       Time.stub(:now).and_return(Time.at(946757701))
       subject.sanitize.to_i.should eq(946757701)
     end
-
   end
 
-  context 'time elapser initialized without parameters' do
-
-    it 'should return a second cero when second_four' do
-      Time.stub(:now).and_return(Time.at(946757704))
-      subject.sanitize(5).to_i.should eq(946757700)
+  context 'time elapser ready for query caching' do
+    it 'should return a second cero for the next 5 seconds' do
+      [946757700, 946757701, 946757702, 946757703, 946757704].each do |local_time|
+        Time.stub(:now).and_return(Time.at(local_time))
+        subject.sanitize(5).to_i.should eq(946757700)
+      end
     end
 
+    it 'should jump to the next window when it has to' do
+      Time.stub(:now).and_return(Time.at(946757705))
+      subject.sanitize(5).to_i.should_not eq(946757700)
+    end
+  end
+
+  context 'time elapser initialized without parameters and rails is used' do
+    before(:each) do
+      RAILS_ROOT = true
+      @time_zone = mock('TimeZone')
+    end
+    it 'should return the current date if no params are provided' do
+      @time_zone.stub(:now).and_return(Time.at(946757701))
+      Time.stub(:zone).and_return(@time_zone)
+      subject.sanitize.to_i.should eq(946757701)
+    end
+
+    it 'should return a second cero for the next 5 seconds' do
+      [946757700, 946757701, 946757702, 946757703, 946757704].each do |local_time|
+        @time_zone.stub(:now).and_return(Time.at(local_time))
+        Time.stub(:zone).and_return(@time_zone)
+        subject.sanitize(5).to_i.should eq(946757700)
+      end
+    end
+
+    it 'should jump to the next window when it has to' do
+      @time_zone.stub(:now).and_return(Time.at(946757706))
+      Time.stub(:zone).and_return(@time_zone)
+      subject.sanitize(5).to_i.should_not eq(946757700)
+    end
   end
 
 end
